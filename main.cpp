d4f5a3e8155fc4182cb8c89ac66d78e854e2d41c
Xiaoxi <shoush0713@gmail.com>
2015-04-22 20:00:14 -0400

    final working commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
-#define INF INT_MAX/2 -1;
+#define INF 99999;
 
 using namespace std;
 
@@ -9,112 +9,78 @@ int main(){
 
     int vertices, edges, queries;
     // Initialize
-    cout<<"Welcome to Floyd-Warshall Program"<<endl;
-    cout<<"Please enter total number of nodes"<<endl;
+    //cout<<"Welcome to Floyd-Warshall Program"<<endl;
+    //cout<<"Please enter total number of nodes"<<endl;
     cin >> vertices;
-    vertices++;
-    cout<<"Please enter total number of Edges"<<endl;
+    //cout<<"Please enter total number of Edges"<<endl;
     cin >> edges;
-    cout<< "Please enter total number of queries."<<endl;
+   // cout<< "Please enter total number of queries."<<endl;
     cin >> queries;
 
-    std::vector<std::vector<int> > graph(
-                                    vertices+1,
-                                    std::vector<int>(vertices+1));
-    std::vector<std::vector<string> > dist(
-                                    vertices+1,
-                                    std::vector<string>(vertices+1));
-/*
-//Initialize both vectors to null
-    for(int i=0; i<vertices ;i++){
-        for(int j =0; j<vertices; j++){
-            graph[i][j]= NULL;
-            dist[i][j] = "";
+    int graph[vertices][vertices];
+    int next[vertices][vertices];
+    //Initialize both vectors to infinity
+    for(int i=1; i<=vertices ;i++){
+        for(int j =1; j<=vertices; j++){
+            //graph[i][j]= NULL;
+            next[i-1][j-1] = 999999999;
+            graph[i-1][j-1] = 999999999;
         }
     }
-*/
+    // initialize diagonal as 0
+    for(int j=1; j <=vertices; j++){
+        graph[j-1][j-1]=0;
+    }
     // initialize distances
     //the dist by default is what's passed in from user
     cout<<"Please enter ABC information"<<endl;
     int a,b,c;
     for(int i=1; i<=edges; i++){
-        stringstream ss;
         cin>>a>>b>>c;
-        ss<<a<<"-"<<b;
-        graph[a][b]=c;
-        dist[a][b] = ss.str();
+        graph[a-1][b-1]=c;
+        next[a-1][b-1] = b;
     }
     // Floyd-Warshall
-    // Add nodes between (first 1 then 2, 3 till n) and look if
-    // distance is shorter
     for(int k = 1; k <=vertices; k++){
         for(int i = 1; i <=vertices; i++){
             for(int j = 1; j <=vertices; j++){
-                if((graph[i][k]*graph[k][j]!=0)&&(i != j)){
-                    if((graph[i][k]+graph[k][j] < graph[i][j])||(graph[i][j]==0)){
+                //if((graph[i-1][k-1]*graph[k-1][j-1]!=0)&&(i != j)){
+                    if((graph[i-1][k-1]+graph[k-1][j-1] < graph[i-1][j-1])){
                         //cout<<k<<endl;
-                         graph[i][j]= graph[i][k]+graph[k][j];
-                        //cout<<"Dist["<<i<<"]["<<j<<"]"<<"Path--"<<i<<" "<<k<<" "<<j<<endl;
-                         stringstream intToString;
-                         if(!dist[i+1][j].empty() && (dist[i][k].length()==3)){
-                             intToString<< i <<"-"<<dist[i+1][j];
-                         }
-                         else if(!dist[i][k].empty()){
-                            intToString << dist[i][k]<<"-"<<j;
-                         }
-                         else{
-                            intToString <<i<<"-"<<k<<"-"<<j;
-                         }
-                         dist[i][j] = intToString.str();
+                         graph[i-1][j-1]= graph[i-1][k-1]+graph[k-1][j-1];
+                         next[i-1][j-1] = next[i-1][k-1];
+               // }
                     }
-                }
             }
         }
     }
-
-    // initialize diagonal as 0
-    for(int j=1; j <=vertices; j++){
-        stringstream ss;
-        ss<<j<<"-"<<j;
-        graph[j][j]=0;
-        dist[j][j] = ss.str();
-    }
-
     //Queries
     int num1, num2;
     cout<<"Please enter your queries"<<endl;
-    for(int x = 0; x< queries; x++){
+    for(int x = 1; x<=queries; x++){
+       // string path; // a temp variable that stores all the string for the path
         cin >> num1;
         cin >> num2;
         //int distance;
-        if(dist[num1][num2].empty()){
+        if(num1 ==num2){
+            cout<<"cost = 0"<<endl<<num1<<"-"<<num2<<endl;
+        }
+        else if(next[num1-1][num2-1]>9999999||graph[num1-1][num2-1]>9999999){
             cout<<"NO Path"<<endl;
         }
-        //else if(graph[num1][num2]== 0){
-            //cout<< "cost = 0 "<<endl<<dist[num1][num2]<<endl;
-        //}
         else{
-            cout<< "cost = "<< graph[num1][num2]<<endl<<dist[num1][num2]<<endl;
+            cout<<"cost = "<<graph[num1-1][num2-1]<<endl;
+            cout<<num1<<"-";
+            while(num1!=num2){
+                num1 = next[num1-1][num2-1];
+                    if(num1==num2) {
+                            cout<<num1<<endl;
+                    }
+                    else {
+                            cout<<num1<<"-";
+                    }
+            }
         }
     }
- //Print out the actual matrix for debugging purposes
-/*
-    cout<<"Path matrix"<<endl;
-    for(int i = 1; i <vertices; i++){
-        for(int j = 1; j <vertices; j++){
-                if(dist[i][j].empty()) cout<<"NONE"<<"\t\t\t";
-                else cout<< dist[i][j]<<"\t\t\t";
-        }
-        cout << endl;
-    }
-    // Print out final distance matrix
-    cout<<"Cost matrix"<<endl;
-    for(int i = 1; i <vertices; i++){
-        for(int j = 1; j<vertices; j++){
-                cout << graph[i][j] << "   ";
-        }
-        cout << endl;
-    }
-*/
-    return 0;
+return 0;
 }
